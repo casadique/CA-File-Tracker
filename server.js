32d35f17de38b52c6b626809cb5355e42789bfc5
casadique <casadique@gmail.com>
2026-07-29 08:43:39 +0530

    added new Derver js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 require("dotenv").config();
 
-const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
